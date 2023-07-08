6776f573346ae1a66f0744c38f87113b64b6d1ed
Rhiannon Moore <rhiannonmoore10@yahoo.com>
2023-07-08 15:24:09 -0700

    completes lab

--- lib/conference_badges.py
+++ lib/conference_badges.py
@@ -1,11 +1,31 @@
 def badge_maker(name):
-    return None
+    return(f"Hello, my name is {name}.")
+
 
 def batch_badge_creator(names):
-    return None
+    return[f'Hello, my name is {name}.' for name in names]
+    
 
 def assign_rooms(names):
-    return None
+    rooms = [1, 2, 3, 4, 5, 6, 7, 8]
+    messages = []
+    for i, name in enumerate(names):
+        if i >= len(rooms):
+            break
+        messages.append(f"Hello, {name}! You'll be assigned to room {rooms[i]}!")
+    return messages
+
+
 
 def printer(names):
-    return None
+    badges = batch_badge_creator(names)
+    assignments = assign_rooms(names)
+    
+    for badge in badges:
+        print(badge)
+    
+    for assignment in assignments:
+        print(assignment)
+    
+result = printer(["Guido", "Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"])
+print(result)
